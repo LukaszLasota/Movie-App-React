2ea7f15a45686d6841c9c96abf913818dfb84d7e
ŁukaszLasota <lukaszlasota89r@gmail.com>
2021-04-07 10:30:13 +0100

    Add files.

---
+++ README.md
@@ -0,0 +1,40 @@
+# Movie App React
+
+This project is a movie app built into React. It uses the API and is built with Create React App.
+
+## Table of contents
+* [How to run the app](#How-to-run-the-app)
+* [Technologies](#technologies)
+
+
+## How to run the app
+
+1\. Clone the repository:
+
+```
+git clone https://github.com/LukaszLasota/
+```
+
+2\. Install the dependencies:
+
+```
+npm install
+```
+
+3\. Runs the app in the development mode.
+
+```
+npm start
+```
+
+4\. Builds the app for production to the `build` folder.
+
+```
+npm run build
+```
+
+## Technologies
+Project is created with:
+* React
+* HTML 5
+* CSS 3
---
+++ src/components/MovieList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const MovieList = (props) =>{
+    const FavouriteComponent = props.favouriteComponent;
+    return(
+        <>
+        {props.movies.map((movie, index) => 
+            <div className='image-container d-flex justify-contetnt-start m-3' key={index} >
+                <img src={movie.Poster} alt="movie" /> 
+                <div onClick={() =>props.handleFavouritesClick(movie)} className="overlay d-flex align-items-center justify-contetnt-center">
+                    <FavouriteComponent />
+                </div>
+            </div>
+        )}
+        </>
+    )
+}
+
+export default MovieList;
